c53b607fe879906eedb4ea718a766009ac88fdf3
carlocattano <cattanoaudioproduction@gmail.com>
2022-05-02 22:27:21 +0200

    setUsername echo

--- setUsername.sh
+++ setUsername.sh
@@ -3,6 +3,8 @@
 
 #for all files in the directory, replace the username with the current user name
 
+echo "setting username to $USER for install script and services"
+
 for f in *; do
     sed -i "s/raspberryUsername/$USER/g" $f
 done
