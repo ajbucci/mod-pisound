7229fb3b49b89831b3e283d0fbf02d1f9e1abfe4
AJ Bucci <ajbucci@gmail.com>
2023-09-01 12:49:20 -0400

    add back mod midi merger

--- startMod.sh
+++ startMod.sh
@@ -3,6 +3,8 @@ set -x
 echo "starting mod..."
 
 sudo service jack start
+sudo service mod-midi-merger start
+sudo service mod-midi-merger-broadcaster start
 sudo service browsepy start
 sudo service mod-host start
 sudo service mod-ui start
--- stopMod.sh
+++ stopMod.sh
@@ -5,4 +5,6 @@ echo "halting mod"
 sudo service mod-ui stop
 sudo service mod-host stop
 sudo service browsepy stop
+sudo service mod-midi-merger-broadcaster start
+sudo service mod-midi-merger start
 sudo service jack stop
